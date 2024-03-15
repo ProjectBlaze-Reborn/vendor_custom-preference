0fab6ba76001e5a1b0a80d7da5f5ca63df65b55a
ryzenforce990 <superxorn@horizondroid.com>
2024-03-15 14:43:33 +0000

    support: Import ColorPreference Settings from crdroid
    Change-Id: Ib014144d719d58f984a54f5249ef96c1152f20c7

---
+++ src/com/android/settings/custom/colorpicker/SecureSettingColorPickerPreference.java
@@ -0,0 +1,26 @@
+package com.android.settings.custom.colorpicker;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
+import com.android.settings.custom.preference.SecureSettingsStore;
+import com.android.settings.custom.colorpicker.ColorPickerPreference;
+public class SecureSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+}
